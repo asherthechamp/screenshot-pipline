47ddb419fcfe5d42f84660068b8af86a5e499a24
Ashenafi <ashukite@gmail.com>
2024-02-12 19:27:54 +0300

    a new one

---
+++ index.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer')
+const path = require('path')
+
+async function takeshot() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(path.join(__dirname, "index.html"))
+    await page.screenshot({
+        path: "./shot.png",
+        fullPage: true
+    })
+    await page.close()
+    await browser.close()
+}
+
+takeshot()
